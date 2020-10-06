345d9109bef5509579f816de5cb03a7a076cd894
Gek Siong Low <geksiong@yahoo.com>
2020-10-06 23:09:00 +0800

    Add support for pathPrefix

--- .eleventy.js
+++ .eleventy.js
@@ -13,44 +13,45 @@ const pagedPostsByTagColl = require("./collections/pagedPostsByTag");
 
 module.exports = (config) => {
 
-    config.addPlugin(pluginTailwind, {
-        src: "src/assets/css/*"
-    });
+  config.addPlugin(pluginTailwind, {
+      src: "src/assets/css/*"
+  });
 
-    config.addPlugin(syntaxHighlight);
-    config.addPlugin(readingTime);
-    config.addPlugin(readerBar);
+  config.addPlugin(syntaxHighlight);
+  config.addPlugin(readingTime);
+  config.addPlugin(readerBar);
 
-    config.setDataDeepMerge(true);
+  config.setDataDeepMerge(true);
 
-    config.addPassthroughCopy("src/assets/js/**/*");
-    config.addPassthroughCopy("src/assets/img/**/*");
-    config.addPassthroughCopy({ "src/posts/img/**/*": "assets/img/" });
+  config.addPassthroughCopy("src/assets/js/**/*");
+  config.addPassthroughCopy("src/assets/img/**/*");
+  config.addPassthroughCopy({ "src/posts/img/**/*": "assets/img/" });
 
-    config.addLayoutAlias('default', 'layouts/default.njk');
-    config.addLayoutAlias('post', 'layouts/post.njk');
+  config.addLayoutAlias('default', 'layouts/default.njk');
+  config.addLayoutAlias('post', 'layouts/post.njk');
 
-    config.addFilter("readableDate", (date) => {
-        return DateTime.fromJSDate(date, { zone: "utc" }).toFormat("d LLLL yyyy hh:mm a");
-    });
+  config.addFilter("readableDate", (date) => {
+      return DateTime.fromJSDate(date, { zone: "utc" }).toFormat("d LLLL yyyy hh:mm a");
+  });
 
-    if (process.env.NODE_ENV === "production") {
-        config.addTransform("minify-html", minifyHTML);
-    }
+  if (process.env.NODE_ENV === "production") {
+      config.addTransform("minify-html", minifyHTML);
+  }
 
-    config.addCollection("posts", postsColl);
-    config.addCollection("tagList", tagListColl);
-    config.addCollection("pagedPosts", pagedPostsColl);
-    config.addCollection("pagedPostsByTag", pagedPostsByTagColl);
+  config.addCollection("posts", postsColl);
+  config.addCollection("tagList", tagListColl);
+  config.addCollection("pagedPosts", pagedPostsColl);
+  config.addCollection("pagedPostsByTag", pagedPostsByTagColl);
 
-    return {
-        dir: {
-            input: "src",
-            output: "dist"
-        },
-        templateFormats: ["md", "njk", "html"],
-        dataTemplateEngine: "njk",
-        markdownTemplateEngine: "njk"
-    }
+  return {
+    pathPrefix: "/gslab/",
+    dir: {
+      input: "src",
+      output: "dist"
+    },
+    templateFormats: ["md", "njk", "html"],
+    dataTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  }
 
 }
