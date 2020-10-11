722bb8feea8993abf5c93ecc5920a4e8f6a46ad8
Gek Siong Low <geksiong@yahoo.com>
2020-10-11 16:21:02 +0800

    Add older/newer links for post templates

--- collections/posts.js
+++ collections/posts.js
@@ -1,5 +1,14 @@
 module.exports = (coll) => {
   const posts = [...coll.getFilteredByGlob("src/content/posts/*.md")];
   posts.reverse();
+
+  // inject newer and older post information into the collection
+  for (let i = 0; i < posts.length; i++) {
+    const olderPost = posts[i + 1];
+    const newerPost = posts[i - 1];
+
+    posts[i].data["olderPost"] = olderPost;
+    posts[i].data["newerPost"] = newerPost;
+  }
   return posts;
 };
