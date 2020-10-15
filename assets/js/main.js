8eac06e062be5df6ba0591116db94574a237b586
Gek Siong Low <geksiong@yahoo.com>
2020-10-15 23:33:34 +0800

    Update website

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,3 +37,91 @@ document.addEventListener(
   },
   false
 );
+
+// medium-zoom on article images
+mediumZoom("#content img", {
+  background: "#333",
+});
+
+/* enhanced code blocks */
+const codeBlocks = document.querySelectorAll("code");
+for (code of codeBlocks) {
+  // toolbar with language and copy function
+  const lang = code.className.replace("language-", "");
+  if (lang) {
+    code.insertAdjacentHTML(
+      "beforebegin",
+      `<div class="code-toolbar">
+        <button class="code-toolbar-item">${lang}</button>
+        <button class="code-toolbar-item btn-copy-code">copy</button>
+      </div>`
+    );
+  }
+  // insert line numbers
+  const lines = code.querySelectorAll(".highlight-line");
+  let counter = 0;
+  for (line of lines) {
+    counter++;
+    line.insertAdjacentHTML(
+      "afterbegin",
+      `<span class="linenumber">${counter}</span>`
+    );
+  }
+}
+
+// attach copy function to "copy" buttons
+new ClipboardJS(".btn-copy-code", {
+  text: function (trigger) {
+    // Remove line numbers from the copy operation
+    // I could only get this to work properly by manipulating the DOM directly. Doesn't work on a cloned node.
+    let code = trigger.parentElement.nextElementSibling;
+    let linenums = code.querySelectorAll(".linenumber");
+    // Hide line numbers
+    for (linenum of linenums) {
+      linenum.style.display = "none";
+    }
+    let copyText = code.innerText;
+
+    // Restore line numbers
+    for (linenum of linenums) {
+      linenum.style.display = "";
+    }
+
+    return copyText;
+  },
+});
+
+// abcjs
+
+if (typeof ABCJS.renderAbc === "function") {
+  let abcSections = document.querySelectorAll(".abcmusic");
+  for (abcSection of abcSections) {
+    // Get the tunes
+    let abcTunes = abcSection.querySelectorAll("p");
+    let tunesText = "";
+    for (abcTune of abcTunes) {
+      tunesText += abcTune.textContent + "\n\n";
+    }
+
+    // Populate tunes menu
+    let tunebook = new ABCJS.TuneBook(tunesText);
+    let tunesSelect = abcSection.querySelector(".tunes-select");
+    for (const [idx, tune] of tunebook.tunes.entries()) {
+      tunesSelect.insertAdjacentHTML(
+        "beforeend",
+        `<option value="${idx}">${tune.title}</option>`
+      );
+    }
+
+    // render to the score area
+    let abcScore = abcSection.querySelector(".abc-score");
+    let renderTune = function (idx) {
+      ABCJS.renderAbc(abcScore, tunebook.tunes[idx].abc);
+    };
+
+    tunesSelect.onchange = function () {
+      renderTune(tunesSelect.value);
+    };
+    renderTune(0);
+  }
+}
